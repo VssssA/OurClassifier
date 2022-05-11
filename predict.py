b9c6da78fc455923d0fd77e55bbe9190a949090d
VssssA <103574122+VssssA@users.noreply.github.com>
2022-05-11 22:28:52 +0500

    Changed paths and some things

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 @click.command()
 @click.argument('image_path', type=click.Path(exists=True))
 def main(image_path):
-    resnet50 = keras.models.load_model('models/resnet50.h5')
+    resnet50 = keras.models.load_model('/content/drive/MyDrive/TrainModel10')
     make_prediction(resnet50, image_path)
 
 """
@@ -17,17 +17,17 @@ def make_prediction(model, path=None):
     if path is None:
         raise UserWarning('Image path should not be None!')
 
-    categories = os.listdir('/content/drive/MyDrive/DatasetVad')
+    categories = os.listdir('/content/drive/MyDrive/testdata10models/test')
 
     # preprocessing
     img = keras.preprocessing.image.load_img(path, target_size=(64, 64))
     x = keras.preprocessing.image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = tf.keras.utils.normalize(x)
-    
+
     # make prediction
     preds = model.predict(x)
-    print("Model predicts a \"{}\" with {:.2f}% probability".format(categories[np.argmax(preds[0])], preds[0][np.argmax(preds)] * 100))
+    print("Model predicts a "{}" with {:.2f}% probability".format(categories[np.argmax(preds[0])], preds[0][np.argmax(preds)] * 100))
 
-if __name__ == '__main__':
+if name == 'main':
     main()
